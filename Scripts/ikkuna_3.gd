99cd8745e173c748df6c282ec0eec0282b76817c
JereIsVeryDumb <smrjere@gmail.com>
2024-08-22 22:37:51 +0300

    redfkg

---
+++ Scripts/ikkuna_3.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@onready var animator = $AnimatedSprite2D
+var can_becleaned = true
+var object_id = "Ikkuna3"
+
+func _ready():
+	GlobalVariables.objects = 0
+	can_becleaned = true
+
+	if GlobalVariables.object_counters.has(object_id):
+		GlobalVariables.object_counters.erase(object_id)
+
+	GlobalVariables.track_object(object_id)
+
+func _process(delta):
+	pass
+
+func _on_area_2d_area_entered(area):
+	if can_becleaned and not GlobalVariables.object_counters[object_id]:
+		animator.play("default")
+		can_becleaned = false
+		GlobalVariables.objects += 1
+		GlobalVariables.object_counters[object_id] = true
+		print(GlobalVariables.objects)
--- Scripts/level_change.gd
+++ Scripts/level_change.gd
@@ -1,12 +1,19 @@
 extends Area2D
 
 
+@onready var Level2 = preload("res://Scenes/level_2.tscn") as PackedScene
+@onready var current_scene = get_tree().current_scene.name
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	pass
+	var current_scene = get_tree().current_scene.name
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if GlobalVariables.objects == 1:
-		pass
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if current_scene == "Level1" and GlobalVariables.objects == 5:
+		get_tree().change_scene_to_file("res://Scenes/level_2.tscn")
